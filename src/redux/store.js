fb5983f9d0686fa992f569e9fb4466fe7b327c3d
analogsoul13 <analogsoul13@gmail.com>
2025-06-13 22:29:46 +0530

    Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../redux/slices/authSlice";
-import appliedJobsReducer from "../redux/slices/appliedJobsSlice"
+import appliedJobsReducer from "../redux/slices/appliedJobsSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,
+} from "redux-persist";
 
-const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        appliedJobs: appliedJobsReducer,
-    },
+const rootReducer = combineReducers({
+  auth: authReducer,
+  appliedJobs: appliedJobsReducer,
 });
 
+const persistedReducer = persistReducer({ key: "root", storage }, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: { ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] },
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
